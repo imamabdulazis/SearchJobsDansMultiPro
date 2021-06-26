95bb1f4bd35b18dca32ea5ab3ec853e0814d50ee
imam <imamabdulazis028@gmail.com>
2021-06-26 08:24:05 +0700

    first commit

---
+++ src/JobsItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const JobsItem = () => {
+  return (
+    <View>
+      <Text>Item</Text>
+    </View>
+  );
+};
+
+export default JobsItem;
+
+const styles = StyleSheet.create({});
